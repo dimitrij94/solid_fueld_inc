874836a0de25cd9b6530e563b0d1a26652e989a7
Dmitrij Kostiushko <dim.kost.94@gmail.com>
2016-08-31 13:48:18 +0300

    Remoeve some redundant code

--- src/main/java/com/example/configuration/SecurityConfig.java
+++ src/main/java/com/example/configuration/SecurityConfig.java
@@ -35,11 +35,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     ObjectMapper objectMapper;
 
-    @Bean
-    public AntPathRequestMatcher antPathRequestMatcher() {
-        return new AntPathRequestMatcher("/login");
-    }
-
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
@@ -48,7 +43,18 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http.csrf().csrfTokenRepository(csrfTokenRepository());
         http.userDetailsService(userDetailsService);
         http
-                .authorizeRequests().antMatchers(HttpMethod.GET, "/admin/**").hasRole(Authorities.SUPER_ADMIN_VAL).antMatchers(HttpMethod.GET, "/admin").hasAnyRole(Authorities.ROLE_ADMIN_VAL, Authorities.SUPER_ADMIN_VAL).antMatchers(HttpMethod.POST, "/admin").hasRole(Authorities.SUPER_ADMIN_VAL).antMatchers(HttpMethod.PUT, "/admin").hasRole(Authorities.SUPER_ADMIN_VAL).antMatchers(HttpMethod.DELETE, "/admin").hasRole(Authorities.SUPER_ADMIN_VAL).antMatchers(HttpMethod.POST, "/order", "/client").permitAll().antMatchers(HttpMethod.GET, "/").hasRole(Authorities.ROLE_ADMIN_VAL).regexMatchers(HttpMethod.POST, "/").hasRole(Authorities.ROLE_ADMIN_VAL).regexMatchers(HttpMethod.DELETE, "/").hasRole(Authorities.ROLE_ADMIN_VAL).regexMatchers(HttpMethod.PUT, "/").hasRole(Authorities.ROLE_ADMIN_VAL).antMatchers(HttpMethod.GET, "/").permitAll();
+                .authorizeRequests()
+                .antMatchers(HttpMethod.GET, "/admin/**").hasRole(Authorities.SUPER_ADMIN_VAL)
+                .antMatchers(HttpMethod.GET, "/admin").hasAnyRole(Authorities.ROLE_ADMIN_VAL, Authorities.SUPER_ADMIN_VAL)
+                .antMatchers(HttpMethod.POST, "/admin").hasRole(Authorities.SUPER_ADMIN_VAL)
+                .antMatchers(HttpMethod.PUT, "/admin").hasRole(Authorities.SUPER_ADMIN_VAL)
+                .antMatchers(HttpMethod.DELETE, "/admin").hasRole(Authorities.SUPER_ADMIN_VAL)
+                .antMatchers(HttpMethod.POST, "/order", "/client").permitAll()
+                .antMatchers(HttpMethod.GET, "/").hasRole(Authorities.ROLE_ADMIN_VAL)
+                .regexMatchers(HttpMethod.POST, "/").hasRole(Authorities.ROLE_ADMIN_VAL)
+                .regexMatchers(HttpMethod.DELETE, "/").hasRole(Authorities.ROLE_ADMIN_VAL)
+                .regexMatchers(HttpMethod.PUT, "/").hasRole(Authorities.ROLE_ADMIN_VAL)
+                .antMatchers(HttpMethod.GET, "/").permitAll();
     }
 
     private CsrfTokenRepository csrfTokenRepository() {
